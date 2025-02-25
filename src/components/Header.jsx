69c152d64a185089dde8c5f8787865ebde25e949
Gabriela <gabrielacacosta@gmail.com>
2025-02-24 19:43:53 -0500

    agregue un cambio al carrito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
+import {React, useContext} from 'react'
 import { Link } from 'react-router'
 import './header.css'
+import { carritoContext } from '../context/ApiContext'
 
 function Header() {
-  return (
-    <header>
-        <div className="logo">
-            <Link to={"/"}>
-            <img src="/logo.png" alt="" />
-            </Link>
-        </div>
-        <nav>
-            <div className="home">
-                <Link to={"/"}><img src="/home.png" alt="" /></Link>
-                <Link to={"/"}>Inicio</Link>
-            </div>
-            <div className="carrito">
-                <Link to={"/Carrito"}>
-                <img src="/carrito.png" alt="" />
+    const {carrito} = useContext(carritoContext)
+    return (
+        <header>
+            <div className="logo">
+                <Link to={"/"}>
+                    <img src="/logo.png" alt="" />
                 </Link>
-                <Link to={"/Carrito"}>Carrito</Link>
             </div>
-        </nav>
-    </header>
-  )
+            <nav>
+                <div className="home">
+                    <Link to={"/"}><img src="/home.png" alt="" /></Link>
+                    <Link to={"/"}>Inicio</Link>
+                </div>
+                <div className="carrito">
+                    <Link to={"/Carrito"}>
+                        <img src="/carrito.png" alt="" />
+                        <span >{ carrito.length}</span>
+                    </Link>
+                    <Link to={"/Carrito"}>Carrito</Link>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
